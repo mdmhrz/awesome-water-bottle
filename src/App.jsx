cb9b6192f6bdb87af1be7ba7c075b643e2ac1637
mdmhrz <razufreelence@gmail.com>
2025-04-16 01:10:44 +0600

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import { Suspense } from 'react'
+import './App.css'
+import Bottles from './components/Bottles/Bottles'
+
+const bottlesPromise = fetch('./bottles.json')
+.then(res => res.json());
+
+function App({}) {
+
+  return (
+    <>
+      <h1>Awesome Water Bottle</h1>
+      <Suspense fallback={<h3>Bottles are loading</h3>}>
+          <Bottles bottlesPromise={bottlesPromise}></Bottles>
+      </Suspense>
+    </>
+  )
+}
+
+export default App
